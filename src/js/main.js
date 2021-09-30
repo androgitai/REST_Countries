04477f3981c82032af8ef06d65bce088265e6f17
Andras Kelemen <andras.kelemen84@gmail.com>
2021-09-30 13:00:26 +0100

    initial webpack setup

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+let items = {
+  coke: 1,
+  chips: 2,
+  chicken: 4,
+};
+
+console.log(items);
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,3 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   entry: './src/js/main.js',
-  module: {
-    rules: [{ test: /\.(js)$/, use: 'babel-loader' }],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main_bundle.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html',
-    }),
-  ],
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 };
